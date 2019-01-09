964e57e96cad6a197feb81dd0a03e3b7475415c8
Leonor Guzman <guzmle2@gmail.com>
2019-01-09 20:50:45 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kas-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'KAS-test';
+}
